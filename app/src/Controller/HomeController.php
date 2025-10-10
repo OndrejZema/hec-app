38b07045fc4473d2efb1b72d9e5eccce7f0d8ba0
ondra <ondra.zema@seznam.cz>
2025-10-10 23:07:49 +0200

    Add logged layout

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -14,18 +14,8 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home', methods: ['GET', 'POST'])]
     public function index(Request $request,): Response
     {
-        $testForm = $this->createForm(TestFormType::class, ['chartData' => [100, 200, 300]]);
-        $testForm->handleRequest($request);
-
-        //dd($testForm);
-        if ($testForm->isSubmitted() && $testForm->isValid()) {
-            dd($testForm->getData()['items']);
-            return $this->redirectToRoute('app_login');
-        }
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'testForm' => $testForm
         ]);
     }
 }
