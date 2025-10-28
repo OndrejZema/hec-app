9aeaf13eefce14230ebef7afa96d5b088387efd2
ondra <ondra.zema@seznam.cz>
2025-10-28 18:23:55 +0100

    Add pagination component

---
+++ app/src/Twig/Components/Pagination.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Pagination
+{
+    public string $routeName = "";
+    public int $page = 1;
+    public int $perPage = 10;
+    public int $lastPage = 0;
+}
